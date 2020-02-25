9ab1741d7c83724e9ea1a30bbd7b756bad17bb02
Vincent <Tianhao_Fu@outlook.com>
2020-02-25 18:23:53 +0800

    Add files via upload

---
+++ 王道考研/三、Hash/Hash统计学生人数.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n ;
+	while(scanf("%d" , &n) != EOF && n != 0)
+	{
+		int Hash[101] = {0}; 
+		for(  int  i = 1 ; i <= n ;  i++ )
+		{
+			int x ;
+			scanf("%d" , &x );
+			Hash[x] ++ ;
+		}
+		
+		int x ;
+		scanf("%d" , &x) ;
+		printf("%d\n" , Hash[x]);	
+	}
+	
+	return 0;
+}
